fbc2d73c4f63b5103ca3c1ac8060d8d4e5293a4a
xies <xies@mit.edu>
2015-07-10 16:59:28 -0400

    better time_of_start handling

--- contractility_models/temporal_models/time_of_start.m
+++ contractility_models/temporal_models/time_of_start.m
@@ -1,10 +1,11 @@
 function C = time_of_start( tis, params )
 %TIME_OF_START Starts nonzero contractility at specified time
 %
-% INPUT: PARAMS:
+% INPUT: PARAMS: 1x2 Cell array
+%        params{1} - cellID
 
 p = zeros(1,numel(tis.getActiveCells));
-p( randi(numel(p),[1 floor(numel(p)/2)]) ) = params(1);
+p( params{1} ) = params{2};
 
 C = [tis.getActiveCells.contractility];
 C = ones(1,numel(C)) * max(C); % HACK!
