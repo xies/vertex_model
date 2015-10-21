1cde3506359ad1a43c59f29565f4c603b9f547c2
xies <xies@mit.edu>
2015-10-20 21:58:43 -0400

    time of start debug

--- contractility_models/temporal_models/time_of_start_linear_increase.m
+++ contractility_models/temporal_models/time_of_start_linear_increase.m
@@ -6,13 +6,13 @@ function C = time_of_start_linear_increase( tis, params )
 %                params{2} - time of start
 %                params{3} - rate of change
 
-p = zeros(1,numel(tis.getActiveCells));
-p( params{1} ) = params{2};
+% p = zeros(1,numel(tis.getActiveCells));
+% p( params{1} ) = params{2};
 
 C = tis.contractile_params.initC + ...
     params{3} * tis.t;
 
-C( p ) = C( p ) - params{2}*params{3};
+C( params{1} ) = C( params{1} ) - params{2}*params{3};
 C = max(C,0);
 
 end
